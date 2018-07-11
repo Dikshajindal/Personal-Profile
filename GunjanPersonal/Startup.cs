4144f74aacbc958d242df8bbf3bce40e35b9d00b
Dikshajindal <gunjangupta9799@gmail.com>
2018-07-12 01:26:24 +0530

    FirstCommitPersonalInfo

---
+++ GunjanPersonal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GunjanPersonal.Startup))]
+namespace GunjanPersonal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
